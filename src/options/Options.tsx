a1df0256d04392c2e51f71cbaa738a0d2e59f3a7
Kolos <kolos7720@gmail.com>
2025-10-18 14:27:49 +0300

    chore: basic layout

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -2,10 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../index.css'
 
+import { Stack, Typography, CssBaseline } from "@mui/material";
+import WebhookInput from "./components/webhook-input.tsx";
+import ListOfVacanciesInput from "./components/list-of-vacancies-input.tsx";
+
 function SidePanel() {
   return (
     <>
-      <h1>Options</h1>
+      <CssBaseline />
+      <Stack spacing={2} padding={3} maxWidth={768}>
+        <Typography variant="h5">LobbyX Scrapper Options</Typography>
+        <WebhookInput />
+        <ListOfVacanciesInput />
+      </Stack>
     </>
   );
 }
