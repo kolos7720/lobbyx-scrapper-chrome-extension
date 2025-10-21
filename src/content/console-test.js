260a8cd96c855bf427e33ebdffbcfb933aa4331e
Kolos <kolos7720@gmail.com>
2025-10-21 19:06:40 +0300

    chore: add tag for scrapped application

---
+++ src/content/console-test.js
@@ -0,0 +1,26 @@
+const application = document.querySelectorAll('[data-candidate]')[0]
+const editButton = application.querySelector('[data-action="vacancies-show#showForm"]')
+editButton.click()
+
+const input = application.querySelector('[data-target="candidate-line.tags"]')
+const tagsContainer = application.querySelector('.tagify')
+
+const tag = "scrapped";
+const currentInputValue = JSON.parse(input.value);
+const newInputValue = [...currentInputValue, { value: tag }];
+
+input.value = JSON.stringify(newInputValue)
+
+const html = `
+  <tag title="${tag}" contenteditable="false" spellcheck="false" value="${tag}">
+    <x title=""></x>
+    <div>
+      <span class="tagify__tag-text">scrapped</span>
+    </div>
+  </tag>
+`
+
+tagsContainer.insertAdjacentHTML('beforeend', html);
+
+const saveButton = application.querySelector('[type="submit"]')
+saveButton.click()
